2b30bdc134bf2baa34357d69f6b1927a5ac27d36
Miiller <miilleramaral@gmail.com>
2017-01-28 01:55:19 -0200

    Controller criado

---
+++ Filtros/Controllers/AutenticadorController.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Mvc;
+using Filtros.Models;
+
+namespace Filtros.Controllers
+{
+    public class AutenticadorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Logar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Logar(Usuario u)
+        {
+            if (u.Username != null && u.Password != null && u.Username.Equals("K19") && u.Password.Equals("K19"))
+            {
+                Session["Usuario"] = u;
+                return RedirectToAction("Index", "Produto");
+            }
+            else
+            {
+                ViewBag.Mensagem = "Usuário ou senha incorretos";
+                return View(u);
+            }
+        }
+
+        public ActionResult Sair()
+        {
+            Session.Abandon();
+            return RedirectToAction("Logar");
+        }
+    }
+}
---
+++ Filtros/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Filtros.Models
+{
+    public class Usuario
+    {
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
